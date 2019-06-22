811d0a4e2dffd115c3dd11b6e90ace5c0ef7627d
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-06-21 18:02:08 -0700

    Remove mistaken merge

--- schema.js
+++
@@ -1,9 +0,0 @@
-type LogMessage = {
-    timestamp: number,
-    filepath: string,
-    lineno: number,
-    level: 'debug' | 'info' | 'warn' | 'error',
-    view: {},
-    logger: string,
-    tags: string[],
-};
