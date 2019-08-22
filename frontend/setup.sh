1352d995511506d9a9ba540334bd369eda44d7a4
Ryan Holmdahl <ryanlh@stanford.edu>
2019-08-22 15:02:38 -0700

    Add `setup.sh` to use `@vizstack/viewer`'s version of `react`
    See https://reactjs.org/warnings/invalid-hook-call-warning.html

---
+++ frontend/setup.sh
@@ -0,0 +1,6 @@
+npm install
+npm update
+npm link ../../vizstack/bindings/typescript
+npm link ../../vizstack/schema/
+npm link ../../vizstack/viewer/
+npm link ../../vizstack/viewer/node_modules/react
