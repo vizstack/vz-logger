f08bd514b4515a3c718717565ee06e8b5b8acf58
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-09-15 20:29:12 -0700

    Fixed setup-links scripts

--- frontend/setup-links.sh
+++ frontend/setup-links.sh
@@ -1,4 +1,4 @@
-npm link ../../vizstack/schema/
-npm link ../../vizstack/bindings/typescript/
-npm link ../../vizstack/viewer/
-npm link ../../vizstack/viewer/node_modules/react
+npm link @vizstack/schema
+npm link @vizstack/js
+npm link @vizstack/viewer
+npm link ../../vizstack/viewer/node_modules/react  # TODO: Remove this.
--- frontend/setup.sh
+++ frontend/setup.sh
@@ -1,3 +1,2 @@
 npm install
-npm update
 bash setup-links.sh
