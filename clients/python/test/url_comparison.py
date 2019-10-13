c3352f7c2efda6ca145643982e4e19131dcc7c3a
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-10-12 19:23:32 -0700

    Update url comparison

--- clients/python/test/url_comparison.py
+++ clients/python/test/url_comparison.py
@@ -3,7 +3,10 @@ from itertools import combinations
 import time
 
 from vzlogger import connect, disconnect, get_logger, DEBUG
-from vizstack import Sequence, Flow, Text
+# from vizstack import Sequence, Flow, Text, KeyValue, Token
+import vizstack as vz
+
+l = get_logger('main').level(DEBUG)
 
 
 def load_urls():
@@ -19,7 +22,7 @@ def get_parameter_differences(url1, url2):
 
     url1_params = url1.split('/')[3:]
     url2_params = url2.split('/')[3:]
-    get_logger('main').debug(url1_params, url2_params)
+    l.info(url1_params, url2_params)
     for i in range(min(len(url1_params), len(url2_params))):
         if url1_params[i] != url2_params[i]:
             mismatches.append((url1_params[i], url2_params[i]))
@@ -29,20 +32,34 @@ def get_parameter_differences(url1, url2):
 
 
 def main():
-    l = get_logger('main').level(DEBUG)
     urls = load_urls()
-    l.debug('All URLS:', urls)
+    l.debug('all URLs:', vz.Sequence(urls, orientation='vertical'))
     url_pairs = combinations(urls, 2)
     off_by_one = []
     for url1, url2 in url_pairs:
         diffs = get_parameter_differences(url1, url2)
-        l.debug(Sequence([
-            Flow(Text('URL pair:', emphasis='more' if len(diffs) == 1 else 'less'), url1, url2),
-            Flow(Text('Mismatches:'), diffs)
-        ], orientation='vertical', show_labels=False))
+        # l.debug(Sequence([
+        #     Flow(Text('URL pair:', emphasis='more' if len(diffs) == 1 else 'less'), url1, url2),
+        #     Flow(Text('Mismatches:'), diffs)
+        # ], orientation='vertical', show_labels=False))
+        # l.debug(vz.KeyValue(
+        #     {
+        #         "pair": vz.Sequence([url1, url2], orientation='vertical'),
+        #         "status": vz.Token('match', color='green') if len(diffs) == 1 else vz.Token('mismatch', color='red'),
+        #         "mismatches": diffs,
+        #     }
+        # , align_separators=True))
+        l.debug(vz.Grid('abc|def', {
+            'a': vz.Text("pair", variant="body"),
+            'b': vz.Text("status", variant="body"),
+            'c': vz.Text('mismatches', variant="body"),
+            "d": vz.Sequence([url1, url2], orientation='vertical'),
+            "e": vz.Token('match', color='green') if len(diffs) == 1 else vz.Token('mismatch', color='red'),
+            "f": diffs,
+        }))
         if len(diffs) == 1:
             off_by_one.append((url1, url2))
-    l.info(Text('Off-by-one:'), Sequence(off_by_one, orientation='vertical'))
+    l.info(vz.Text('Off-by-one:'), vz.Sequence(off_by_one, orientation='vertical'))
 
 
 
